8114e481cb2371c37532326bb7ca0c45089a5430
Philip osir Otieno <philiposir2@gmail.com>
2024-08-04 05:00:46 +0300

    updates

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,20 @@
 // Code EyesOnMe Component Here
+
+import React from "react";
+
+function EyesOnMe() {
+
+  function tohundlefocus(event) {
+    console.log("Good!");
+  }
+
+  function tohundleblur(event) {
+    console.log('Hey! Eyes on me!')
+  }
+
+  return(
+    <button onFocus={tohundlefocus} onBlur={tohundleblur}>Eyes on me</button>
+  )
+}
+
+export default EyesOnMe
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,9 +1,13 @@
-// Code Keypad Component Here
+import React from 'react';
 
-function Keypad (){
-    return (
-        <div></div>
-    )
+function Keypad() {
+  function handleinput(event) {
+    console.log("Entering password...");
+  }
+
+  return (
+    <input type='password' name='password' onChange={handleinput} />
+  );
 }
 
 export default Keypad;
